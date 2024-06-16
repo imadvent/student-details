724dc9f3c0a933407aaa721c26e6704167a7ce6e
imadvent <advent@duck.com>
2024-06-16 14:55:23 +0530

    added swagger-ui

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -3,6 +3,10 @@ package com.student.controller;
 import com.student.dto.StudentRequest;
 import com.student.dto.StudentResponse;
 import com.student.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,27 +14,46 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Students", description = "the Students List")
 @RestController
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
-    @PostMapping(value = "/insert")
+    @Operation(
+            summary = "Insert students data",
+            description = "insert all students data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
+    @PostMapping(value = "/insert", produces = "application/json")
     public ResponseEntity<?> create(@RequestBody StudentRequest studentRequest) {
 
         StudentResponse studentResponse = studentService.insert(studentRequest);
         return new ResponseEntity<>(studentResponse, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/viewall")
+    @Operation(
+            summary = "Fetch all students",
+            description = "fetches all students data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
+    @GetMapping(value = "/viewall", produces = "application/json")
     public ResponseEntity<?> readAll() {
 
         List<StudentResponse> studentResponse = studentService.viewAll();
         return new ResponseEntity<>(studentResponse, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/view/{studentId}")
+    @Operation(
+            summary = "Fetch students",
+            description = "fetches students data by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
+    @GetMapping(value = "/view/{studentId}", produces = "application/json")
     public ResponseEntity<?> read(@PathVariable("studentId") int studentId) {
 
         StudentResponse studentResponse = studentService.view(studentId);
@@ -38,14 +61,27 @@ public class StudentController {
         return new ResponseEntity<>(studentResponse, HttpStatus.OK);
     }
 
-    @PutMapping(value = "/change/{studentId}")
-    public ResponseEntity<?> update(@PathVariable("studentId") int studentId, @RequestBody StudentRequest studentRequest) {
+    @Operation(
+            summary = "update students",
+            description = "update students data by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
+    @PutMapping(value = "/change/{studentId}", produces = "application/json")
+    public ResponseEntity<?> update(@PathVariable("studentId") int studentId,
+                                    @RequestBody StudentRequest studentRequest) {
 
         StudentResponse studentResponse = studentService.change(studentId, studentRequest);
 
         return new ResponseEntity<>(studentResponse, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "delete students",
+            description = "delete students data by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
     @DeleteMapping(value = "/remove/{studentId}")
     public ResponseEntity<?> delete(@PathVariable("studentId") int studentId) {
 
